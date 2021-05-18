6cd0abef33990303c2fc89b0f6d352c4b5cf479a
Gopal Subramaniyan V R <subramaniang573@gmail.com>
2021-05-18 14:45:10 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const ApiKey = 'AIzaSyA7I9pVEE5qJwEbvxTEHCpHQDEuePtj8uo';
+
+const OAuthId = '756283618964-mu7c6q41e3sgkr95mt6egsukt0dj6h5m.apps.googleusercontent.com';
+
+const url = `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&forUsername=GoogleDevelopers&key=${ApiKey}`;
+
+async function oauthSignIn(){
+    const accessTokenReq = `https://accounts.google.com/o/oauth2/v2/auth?scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fyoutube.readonly&include_granted_scopes=true&state=state_parameter_passthrough_value&redirect_uri=http://localhost&response_type=token&client_id=${OAuthId}`;
+    
+    const data = await fetch(accessTokenReq,{method:'GET',mode:"no-cors"});
+
+    console.log(data);
+}
+
+oauthSignIn();
