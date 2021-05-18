d66cc0037f02c37043feb28457165a10d3036300
Gopal Subramaniyan V R <subramaniang573@gmail.com>
2021-05-18 21:23:58 +0530

    'test'

--- script.js
+++ script.js
@@ -155,7 +155,7 @@ function createdPlaylist(sysGeneratedPlaylist){
 
         //loop through videos
         sysGeneratedPlaylist.forEach(item => {
-            console.log(item)
+            console.log(`https://www.youtube.com/embed/videoseries?list=${item}`);
             output += `
                 <div class="col s3">
                     <iframe width="100%" height="auto" src="https://www.youtube.com/embed/videoseries?list=${item}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
