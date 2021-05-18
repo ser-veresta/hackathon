d7a227ff9735b013a7a502ff1d4e97799778fb1e
Gopal Subramaniyan V R <subramaniang573@gmail.com>
2021-05-18 18:43:51 +0530

    'test'

--- script.js
+++ script.js
@@ -128,7 +128,7 @@ function requestVideoplaylist(playlistId){
 
                 output += `
                     <div class="col s3">
-                        <iframe width="100%" height="auto" src="https://youtube.com/embed/${videoId}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
                 `;
             });
