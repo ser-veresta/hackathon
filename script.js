f47aaa207ba61b7cd991c0768cdccbe9d224f851
Gopal Subramaniyan V R <subramaniang573@gmail.com>
2021-05-18 15:06:18 +0530

    test

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const url = `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2Cc
 async function oauthSignIn(){
     const accessTokenReq = `https://accounts.google.com/o/oauth2/v2/auth?scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fyoutube.readonly&include_granted_scopes=true&state=state_parameter_passthrough_value&redirect_uri=http://localhost&response_type=token&client_id=${OAuthId}`;
     
-    const data = await fetch(accessTokenReq,{method:'GET'});
+    const data = await fetch(accessTokenReq,{method:'GET',headers:{Origin: 'https://quirky-keller-bcfa0c.netlify.app'}});
 
     console.log(data.json());
 }
