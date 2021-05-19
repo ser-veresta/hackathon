852876d101c9cbb498737e1501da5ced00cbe232
Gopal Subramaniyan V R <subramaniang573@gmail.com>
2021-05-19 09:31:22 +0530

    'test'

--- script.js
+++ script.js
@@ -453,7 +453,7 @@ function searchfunctionality(id){
                     output += `
                     <div class="col s12">
                         <h6>${item.snippet.title}</h6>
-                        <iframe width="100%" height="auto" src="https://www.youtube.com/embed/${videoId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+                        <iframe width="100%" height="500px" src="https://www.youtube.com/embed/${videoId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
                         </iframe>
                     </div>
                 `;
